9ea60a8f7325a7d2f249112d98836afaf8cd03d7
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-01-07 18:29:01 -0300

    Logged page and Authentication

--- src/Pages/Logged/index.js
+++ src/Pages/Logged/index.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { AuthGoogleContext } from '../../Contexts/authGoogle'
 import './Logged-Style.css';
+import Save from '../../images/save.png';
+import Out from '../../images/out.png';
 
 export default () => {
     const { user , signOut , signed } = useContext(AuthGoogleContext);
@@ -22,6 +24,7 @@ export default () => {
         <div className="logged">
             <header>
                 <div className="logo">
+                    <img src={Save} alt="logo" /> 
                     <h2>M_Mannager</h2>
                 </div>
                 <div className="user">
@@ -32,45 +35,52 @@ export default () => {
             <main>
                 <div className="show">
                     <div className="showA">
-                        <p>saldo : <span className="saldo"></span></p>
+                        <p>saldo : <span className="saldo">R$500</span></p>
                         <div className="relacion">
-                            <div></div>
-                            <div></div>
+                            <div className="full"></div>
                         </div>
                     </div>
                     
                     <div className="showB">
-                        <button className="dividas">ver Dívidas</button>
-                        <button className="lucros">ver Renda</button>
+                        <button className="dividas">show debts</button>
+                        <button className="lucros">show income</button>
                     </div>
                 </div>
 
                 <div className="add">
-                    <button className="addDividas">Add Dívidas</button>
-                    <button className="addLucros">Add Renda</button>
+                    <button className="addDebts">Add debts</button>
+                    <button className="addIncomes">Add income</button>
+                    <button className="pay">pay debt</button>
                 </div>
 
                 <div className="history">
-                    history
+                    <h2>History</h2>
+                    <div className="history-children">
+
+                    </div>
                 </div>
 
-                <div className="savings">
-                    <button className="modifyGoal">Goal</button>
-                    <p>Goal : 100</p>
+                <div className="savings"> 
+                    <button className="setGoal">Set Goal</button>
+                    <h2>My Savings</h2>
+                    <div>
+                        <p>Goal : <span className="goal">R${12}</span></p>
+
+                    </div>
                     <div className="conclued">
-                        <p className="conc">50%</p>
+                        <p className="conc">{50}%</p>
                         <div></div>
                         <div></div>
                     </div>
-                    <div className="savButts">
-                    <button className="saveAdd">Add Savings</button>
-                    <button className="saveRemove">Del Savings</button>
+                    <div className="savingButts">
+                        <button className="removeSave">Del Savings</button>
+                        <button className="addSave">Add Savings</button>
                     </div>
                 </div>
             </main>
 
             <footer>
-                <button className="logout" onClick={logOut}>logOut</button>
+                <img src={Out} alt="out img" className="out" onClick={logOut}/>
                 <p>Made with React and Firebase</p>
             </footer>
         </div>
